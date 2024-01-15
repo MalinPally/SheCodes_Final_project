336d562167ad158198ed0771c21bbede18b5f887
Malin Bengtsson <149403295+MalinPally@users.noreply.github.com>
2024-01-15 22:26:43 +0100

    JS CB

--- src/index.js
+++ src/index.js
@@ -65,37 +65,66 @@ function formatDate(date) {
   return `${day}, ${hours}:${minutes}`;
 }
 
-function displayForecast() {
-  let forecastElement = document.querySelector("#forecast");
-  let forecast = document.querySelector("#forecast");
-  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+function displayForecast(response) {
   let forecastHtml = "";
 
-  days.forEach(function (day) {
-    forecastHtml += `
-      <div class="weather-forecast" id="forecast">
-        <div class="weather-forecast-day">
-          <div class="weather-forecast-date">${day}</div>
-          <div class="weather-forecast-icon">☁️</div>
-          <div class="weather-forecast-temperatures">
-            <div class="weather-forecast-temperature-max">
-              <strong>15°</strong>
-            </div>
-            <div class="weather-forecast-temperature-min">9°</div>
-          </div>
-        </div>
-      </div>
-    `;
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml += `<div class="weather-forecast-day">
+                <div class="weather-forecast-date">${formatDay(day.time)}</div>
+
+                <img src="${
+                  day.condition.icon_url
+                }" class="weather-forecast-icon" />
+
+                <div class="weather-forecast-temperatures">
+                    <div class="weather-forecast-temperature">
+                        <strong>${Math.round(day.temperature.maximum)}º</strong>
+                    </div>
+                    <div class="weather-forecast-temperature">${Math.round(
+                      day.temperature.minimum
+                    )}º</div>
+                </div>
+            </div>`;
+    }
   });
 
-    forecastElement.innerHTML = forecastHtml;
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
 }
 
-let searchForm = document.querySelector("#search-form");
-searchForm.addEventListener("submit", search);
+function searchCity(city) {
+  let apiKey = "66ao30d4c3f4t8b09259fcd03dac689e";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(updateWeatherInformation);
+}
 
-let currentDateElement = document.querySelector("#current-date");
-let currentDate = new Date();
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-input");
+  let cityElement = document.querySelector("#current-city");
 
-currentDateElement.innerHTML = formatDate(currentDate);
-window.addEventListener("load", fetchDataForDefaultCity);
+  cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
+}
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+
+  return days[date.getDay()];
+}
+
+function getForecast(city) {
+  let apiKey = "66ao30d4c3f4t8b09259fcd03dac689e";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}`;
+  axios(apiUrl).then(displayForecast);
+  console.log(apiUrl);
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Malmö");
+
+fetchDataForDefaultCity();
