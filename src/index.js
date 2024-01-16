b47668cfc0875e5651dc78d5450dbde5f481ed2c
Malin Bengtsson <149403295+MalinPally@users.noreply.github.com>
2024-01-16 01:28:29 +0100

    Nu?

--- src/index.js
+++ src/index.js
@@ -37,6 +37,7 @@ function fetchDataForDefaultCity() {
   });
 }
 
+
 function search(event) {
   event.preventDefault();
   let searchInputElement = document.querySelector("#search-input");
@@ -117,6 +118,33 @@ function handleSearchSubmit(event) {
   cityElement.innerHTML = searchInput.value;
   search(searchInput.value);
 }
+
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+ let apiKey = "66ao30d4c3f4t8b09259fcd03dac689e";
+ let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+
+  searchCity(searchInput.value);
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Paris");
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
