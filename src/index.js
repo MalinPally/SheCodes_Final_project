cdf5ec0b22b805acc54fa9e9664c2c079f9bd6e9
Malin Bengtsson <149403295+MalinPally@users.noreply.github.com>
2024-01-16 01:23:09 +0100

    Nu orkar jag inte mer

--- src/index.js
+++ src/index.js
@@ -1,36 +1,78 @@
-function searchCity(city) {
-  let apiKey = "66ao30d4c3f4t8b09259fcd03dac689e";
-  let currentApiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-  let forecastApiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+function displayEverything(response) {
+  let temperatureElement = document.querySelector("#current-temperature");
+  let temperature = Math.round(response.data.temperature.current);
+  let cityElement = document.querySelector("#current-city");
+  let windElement = document.querySelector("#windElement");
+  let humidityElement = document.querySelector("#humidityElement");
+  let conditionElement = document.querySelector("#conditionElement");
+  let iconElement = document.querySelector("#iconElement");
 
-  axios.get(currentApiUrl).then(updateWeather);
-  axios.get(forecastApiUrl).then(displayForecast);
+  let wind = Math.round(response.data.wind.speed);
+  let humidity = Math.round(response.data.temperature.humidity);
+  let condition = response.data.condition.description;
+  let iconUrl = response.data.condition.icon_url;
+  let iconDescription = response.data.condition.icon;
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = temperature;
+  windElement.innerHTML = wind + " km/h";
+  humidityElement.innerHTML = humidity + "%";
+  conditionElement.innerHTML = condition;
+
+  iconElement.src = iconUrl;
+  iconElement.alt = iconDescription;
+
+  let weatherDetails = document.getElementById("weatherDetails");
+  weatherDetails.style.display = "block";
 }
 
-let defaultCity = "Malmö";
+function fetchDataForDefaultCity() {
+  let defaultCity = "Malmö";
+  let apiKey = "66ao30d4c3f4t8b09259fcd03dac689e";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${defaultCity}&key=${apiKey}&units=metric`;
 
-function updateWeather(response) {
-  let temperatureElement = document.querySelector("#temperature");
-  let temperature = response.data.temperature.current;
-  let humidityElement = document.querySelector("#humidity");
-  let windSpeedElement = document.querySelector("#wind-speed");
-  let timeElement = document.querySelector("#time");
-  let iconElement = document.querySelector("#icon");
+  axios.get(apiUrl).then((response) => {
+    displayEverything(response);
+    getForecast(defaultCity);
+  });
+}
 
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = response.data.city;
-  temperatureElement.innerHTML = Math.round(temperature);
-  let descriptionElement = document.querySelector("#description");
+function search(event) {
+  event.preventDefault();
+  let searchInputElement = document.querySelector("#search-input");
+  let city = searchInputElement.value;
+  let apiKey = "66ao30d4c3f4t8b09259fcd03dac689e";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
-  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="current-temperature-icon" />`;
+  axios.get(apiUrl).then((response) => {
+    displayEverything(response);
+    getForecast(city);
+  });
+}
 
-  descriptionElement.innerHTML = response.data.condition.description;
-  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
-  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
-  timeElement.innerHTML = formalDate(new Date(response.data.time * 1000));
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day}, ${hours}:${minutes}`;
 }
 
 function displayForecast(response) {
+  let forecastElement = document.querySelector("#forecast");
   let forecastHtml = "";
 
   response.data.daily.forEach(function (day, index) {
@@ -38,7 +80,6 @@ function displayForecast(response) {
       forecastHtml += `
         <div class="weather-forecast-day">
           <div class="weather-forecast-date">${formatDay(day.time)}</div>
-  
           <img src="${day.condition.icon_url}" class="weather-forecast-icon" />
           <div class="weather-forecast-temperatures">
             <div class="weather-forecast-temperature">
@@ -48,31 +89,40 @@ function displayForecast(response) {
               day.temperature.minimum
             )}º</div>
           </div>
-        </div>
-      `;
+        </div>`;
     }
   });
 
-  let forecastElement = document.querySelector("#forecast");
   forecastElement.innerHTML = forecastHtml;
+}
+
+function getForecast(city) {
+  let apiKey = "66ao30d4c3f4t8b09259fcd03dac689e";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function formatDay(timestamp) {
   let date = new Date(timestamp * 1000);
-  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-
+  let days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
   return days[date.getDay()];
 }
 
 function handleSearchSubmit(event) {
   event.preventDefault();
-  let searchInput = document.querySelector("#search-form-input");
+  let searchInput = document.querySelector("#search-input");
+  let cityElement = document.querySelector("#current-city");
 
-  searchCity(searchInput.value);
+  cityElement.innerHTML = searchInput.value;
+  search(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
-// Initially load weather for the default city
-searchCity(defaultCity);
+let currentDateElement = document.querySelector("#current-date");
+let currentDate = new Date();
+
+currentDateElement.innerHTML = formatDate(currentDate);
+window.addEventListener("load", fetchDataForDefaultCity);
